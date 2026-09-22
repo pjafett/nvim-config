05cd15eb04996206241c95d49875361a8eca9af7
Jafett Puga <pugajaf@gmail.com>
2026-09-21 22:07:40 -0500

    Add switching between buffer splits with ctrl-hjkl

--- init.lua
+++ init.lua
@@ -15,3 +15,8 @@ vim.keymap.set('n', '<leader>p', ':bprev<CR>')
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left split' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to below split' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to above split' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right split' })
